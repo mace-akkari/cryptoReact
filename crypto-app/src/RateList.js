2ac60e337fee088344e0f70b303bc3b2fe1d5484
Mace <maceplus@gmail.com>
2019-06-12 15:51:04 +0100

    new border & getlist function

--- crypto-app/src/RateList.js
+++ crypto-app/src/RateList.js
@@ -2,17 +2,26 @@ import React from 'react';
 import './RateList.css';
 
 const getRate = (rate) => (
-    <li key={rate.currency}>
+    <li className="ratesBorder" key={rate.currency}>
         <p>{rate.currency}</p>
         <p>{rate.value}</p>
     </li>
 );
 
+const getList = (rates) => {
+    if (rates == null) {
+        return "No rates available";
+    } else if (rates.length === 0) {
+        return "Empty rates list"
+    } else {
+        return rates.map(getRate)
+    }
+}
 const RateList = (props) => {
     const { rates } = props;
     return (
         <ul className="rate-list">
-            {rates.map(getRate)}
+            {getList(rates)}
         </ul>
     );
 }
